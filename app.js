a2a75443321da004cbae8227c8ae836f514fa0e4
rlynn523 <ryanlynn523@gmail.com>
2016-06-07 11:17:14 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+$(function(){
+	var Pantry = {
+		Strong = ["glug of rum", "slug of whisky", "splash of gin"],
+		Salty = ["olive on a stick", "salt-dusted rim", "rasher of bacon"],
+		Bitter = ["shake of bitters", "splash of tonic", "twist of lemon peel"],
+		Sweet = ["sugar cube", "spoonful of honey", "splash of cola"],
+		Fruity = ["slice of orange", "dash of cassis", "cherry on top"]
+	}
+}
